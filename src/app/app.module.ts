20132b0fc3c8faf2df92a4ee6ce1e6eaae27b056
Adekams <kifademi@gmail.com>
2024-01-14 09:06:14 +0100

    craftmanship section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TopSellersComponent } from './components/top-sellers/top-sellers.component';
+import { CraftmanshipComponent } from './components/craftmanship/craftmanship.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopSellersComponent,
+    CraftmanshipComponent
   ],
   imports: [
     BrowserModule,
